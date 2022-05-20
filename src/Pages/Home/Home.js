e9433bc82522213124afb95cabaef46cf27e1128
git-mhasan <mahadi2020dev@gmail.com>
2022-05-20 20:15:58 +0600

    first commit todo client

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='d-flex justify-center align-middle'>
+            <button className='btn btn-primary'>Add New Task</button>
+        </div>
+    );
+};
+
+export default Home;
